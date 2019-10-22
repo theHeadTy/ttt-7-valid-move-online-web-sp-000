cf8248d30a98d0f83af0e833ffa8e63f16ef04f3
generic-puppet-7755 <tasmith1990@gmail.com>
2019-10-22 22:24:48 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,34 @@
+
 # code your #valid_move? method here
-
-
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def valid_move?(board, index)
+  
+  # if index is over 9 end.
+  if !index.between?(0, 8)
+    return nil
+  end
+  
+  # if the new position is already taken end.
+  if board.at(index) != " "
+    return false
+  end
+  
+  # If the board is empty and the new position is valid on board.
+  if board.all? { |i| i == " " } && index.between?(0, 8)
+    return true
+  else
+    return true # post the move.
+  end
+    
+    
+end
+
+board = Array.new(9, " ")
+
+# empty board
+valid_move?(board, 99)
+
+valid_move?([" ", " ", "X", " ", " ", " ", " ", "O", " "], 5)
+
+
