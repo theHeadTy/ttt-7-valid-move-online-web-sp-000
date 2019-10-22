966c46afc0107232f20f614c3c5048a0f6de9cff
generic-puppet-7755 <tasmith1990@gmail.com>
2019-10-22 22:22:28 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -18,7 +18,7 @@ def valid_move?(board, index)
   if board.all? { |i| i == " " } && index.between?(0, 9)
     return true
   else
-    return true
+    return true # board not empty
   end
     
     
